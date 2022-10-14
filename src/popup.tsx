b9e0351b5d49e34654dacdf48680c59861bc6e5f
Takuya Eguchi <seal2501@gmail.com>
2022-10-14 14:36:38 +0900

    feat: 実際にChromeExtension上でReactっぽい処理ができることを確認

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const Popup = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    chrome.storage.sync.get(null, (options) => {
+      if (options.currentCount !== undefined) {
+        setCount(options.currentCount);
+      }
+    });
+  }, []);
+
+  const countUp = async () => {
+    const newCOunt = count + 1;
+    setCount(newCOunt);
+    await chrome.storage.sync.set({
+      currentCount: newCOunt,
+    });
+  };
+
+  const countDown = async () => {
+    const newCOunt = count - 1;
+    setCount(newCOunt);
+    await chrome.storage.sync.set({
+      currentCount: newCOunt,
+    });
+  };
+
   return (
     <>
-      <h1>test</h1>
+      <h1>Counter</h1>
       <div>
-        <button>Click me</button>
+        <p>{count}</p>
+        <button
+          onClick={() => countUp()}
+        >+</button>
+        <button
+          onClick={() => countDown()}
+        >-</button>
       </div>
     </>
   );
