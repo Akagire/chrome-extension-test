6869b314957811a06cfd9a98521eb8ca87717568
Takuya Eguchi <seal2501@gmail.com>
2022-10-14 11:29:35 +0900

    chore: WebpackとTypeScriptの設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,42 @@
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  mode: 'development',
+  entry: {
+    popup: './src/popup.tsx',
+    options: './src/options.tsx',
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/react',
+              ],
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  target: 'web',
+};
